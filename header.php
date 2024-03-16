85d8fd90e6e891053678bb10b05a1a981243317b
DESKTOP-060LROS\Inspira Grupa <dusanstanic2@gmail.com>
2024-03-16 21:42:27 +0100

    stavljen link ka postingu

--- header.php
+++ header.php
@@ -50,7 +50,7 @@ session_start();
                             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">List Your Room</a>
+                            <a class="nav-link" href="posting.php">List Your Room</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="looking.php">Find Your Room</a>
