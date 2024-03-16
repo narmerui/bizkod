3409e9ec8a5ed7e4c15fd124e0b170aea282658a
Narmi <kujundzicmatej64@gmail.com>
2024-03-16 16:52:29 +0100

    no message

--- header.php
+++ header.php
@@ -40,14 +40,14 @@ session_start();
         <a href="index.php"></i></a>
         <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary mb-2">
             <div class="container-fluid">
-                <a class="navbar-brand px-5" href="#"><i class="bi bi-house"></i></a>
+                <a class="navbar-brand px-5" href="index.php"><i class="bi bi-house"></i></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">List Your Room</a>
--- signupflatseek.php
+++ signupflatseek.php
@@ -3,22 +3,6 @@ include_once "header.php"
 ?>
 
 <section>
-    <h2>Sign Up</h2>
-    <form action="includes/signupflatseek.inc.php" method="post">
-        <input type="text" name="name" placeholder="Name...">
-        <input type="text" name="surname" placeholder="Surname...">
-        <input type="date" name="birth_date" placeholder="Birth date...">
-        <select name="gender" id="gender">
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-        </select>
-        <input type="text" name="email" placeholder="Email...">
-        <input type="text" name="phone" placeholder="Phone number...">
-        <input type="text" name="university" placeholder="University...">
-        <input type="password" name="pwd" placeholder="Password...">
-        <input type="password" name="pwdrepeat" placeholder="Repeat password...">
-        <button type="submit" name="submit">Sign Up</button>
-    </form>
     <section class="vh-100" style="background-color: #eee;">
         <div class="container h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
@@ -30,20 +14,21 @@ include_once "header.php"
 
                                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                                    <form class="mx-1 mx-md-4">
+                                    <form class="mx-1 mx-md-4" action="includes/signupflatseek.inc.php" method="post">
 
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="formFirstName" class="form-control" />
+                                                <input type="text" id="formFirstName" name="name" class="form-control" />
                                                 <label class="form-label" for="formFirstName">First Name</label>
                                             </div>
                                         </div>
 
+
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="formSurname" class="form-control" />
+                                                <input type="text" name="surname" id="formSurname" class="form-control" />
                                                 <label class="form-label" for="formSurname">Surname</label>
                                             </div>
                                         </div>
@@ -51,7 +36,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="email" id="formEmail" class="form-control" />
+                                                <input type="email" name="email" id="formEmail" class="form-control" />
                                                 <label class="form-label" for="formEmail">Your Email</label>
                                             </div>
                                         </div>
@@ -59,7 +44,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="formPassword" class="form-control" />
+                                                <input type="password" name="pwd" id="formPassword" class="form-control" />
                                                 <label class="form-label" for="formPassword">Password</label>
                                             </div>
                                         </div>
@@ -67,7 +52,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="formRepeatPassword" class="form-control" />
+                                                <input type="password" name="pwdrepeat" id="formRepeatPassword" class="form-control" />
                                                 <label class="form-label" for="formRepeatPassword">Repeat your password</label>
                                             </div>
                                         </div>
@@ -75,7 +60,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="tel" id="formPhone" class="form-control" />
+                                                <input type="tel" name="phone" id="formPhone" class="form-control" />
                                                 <label class="form-label" for="formPhone">Phone Number</label>
                                             </div>
                                         </div>
@@ -83,7 +68,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-venus-mars fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <select id="formGender" class="form-select">
+                                                <select id="formGender" name="gender" class="form-select">
                                                     <option value="">Choose Gender</option>
                                                     <option value="male">Male</option>
                                                     <option value="female">Female</option>
@@ -95,7 +80,7 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-university fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="formUniversity" class="form-control" />
+                                                <input type="text" name="university" id="formUniversity" class="form-control" />
                                                 <label class="form-label" for="formUniversity">University</label>
                                             </div>
                                         </div>
@@ -103,20 +88,20 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-birthday-cake fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="date" id="formBirthdate" class="form-control" />
-                                                <label class="form-label" for="formBirthdate">Birthdate</label>
+                                                <input type="date" name="birth_date" id="formBirthdate" class="form-control" />
+                                                <label class="form-label" for="formBirthdate"></label>
                                             </div>
                                         </div>
 
                                         <div class="form-check d-flex justify-content-center mb-5">
                                             <input class="form-check-input me-2" type="checkbox" value="" id="formTerms" />
                                             <label class="form-check-label" for="formTerms">
-                                                I agree to all statements in <a href="#!">Terms of service</a>
+                                                I agree to all statements in <a href="#!">Terms of Service</a>
                                             </label>
                                         </div>
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="button" class="btn btn-primary btn-lg">Register</button>
+                                            <button type="submit" name="submit" class="btn btn-primary btn-lg">Register</button>
                                         </div>
 
                                     </form>
@@ -124,11 +109,39 @@ include_once "header.php"
                                 </div>
                                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                                         class="img-fluid" alt="Sample image">
+                                    <img src="images/firstback.jpg" class="img-fluid rounded" alt="Sample image">
 
                                 </div>
                             </div>
+
+                            <?php
+                            if(isset($_GET["error"])){
+
+                                switch ($_GET["error"]) {
+                                    case "emptyinput":
+                                        echo "<p class='text-center fs-3'>Fill in all fields!</p>";
+                                        break;
+//        case "invalidUid":
+//            echo "<p>Choose a proper username!</p>";
+//            break;
+                                    case "invalidEmail":
+                                        echo "<p>Choose a proper email!</p>";
+                                        break;
+                                    case "passwordsdontmatch":
+                                        echo "<p>Passwords doesn't match!</p>";
+                                        break;
+                                    case "stmtfailed":
+                                        echo "<p>Something went wrong, try again!</p>";
+                                        break;
+                                    case "emailtaken":
+                                        echo "<p>User with this email already exists!</p>";
+                                        break;
+                                    case "none":
+                                        header("Location: looking.php");
+                                        exit();
+                                }
+                            }
+                            ?>
                         </div>
                     </div>
                 </div>
@@ -137,34 +150,6 @@ include_once "header.php"
     </section>
 
 </section>
-<?php
-if(isset($_GET["error"])){
-
-    switch ($_GET["error"]) {
-        case "emptyinput":
-            echo "<p>Fill in all fields!</p>";
-            break;
-//        case "invalidUid":
-//            echo "<p>Choose a proper username!</p>";
-//            break;
-        case "invalidEmail":
-            echo "<p>Choose a proper email!</p>";
-            break;
-        case "passwordsdontmatch":
-            echo "<p>Passwords doesn't match!</p>";
-            break;
-        case "stmtfailed":
-            echo "<p>Something went wrong, try again!</p>";
-            break;
-        case "emailtaken":
-            echo "<p>User with this email already exists!</p>";
-            break;
-        case "none":
-            header("Location: looking.php");
-            exit();
-    }
-}
-?>
 <?php
 include_once "footer.php"
 ?>
