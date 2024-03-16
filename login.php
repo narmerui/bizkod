ca5a603dec220798a381fa940e9e8e276a761797
IDK616\inspira <duledule17113@gmail.com>
2024-03-16 11:58:02 +0100

    no message

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include_once "header.php"
     <section>
         <h2>Log In</h2>
         <form action="includes/login.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username/Email...">
+            <input type="text" name="uid" placeholder="Email...">
             <input type="password" name="pwd" placeholder="Password...">
             <button type="submit" name="submit">Log In</button>
         </form>
