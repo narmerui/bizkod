15d5811f028f7d7e7f584460081279a2eb8f421b
IDK616\inspira <duledule17113@gmail.com>
2024-03-16 13:48:56 +0100

    login cleanup

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,39 +1,37 @@
 <?php
-if(isset($_POST["submit"])){
-    $email = $_POST["uid"];
+if(isset($_POST["submit"])) {
 
-$email = $_POST["email"];
-$pass = $_POST["pwd"];
-require_once "dbh.inc.php";
-require_once "functions.inc.php";
+    $email = $_POST["email"];
+    $pass = $_POST["pwd"];
 
-if(emptyInputLogIn($email,$pass)){
-    header("Location: ../login.php?error=emptyinput");
-    if(emptyInputLogIn($email, $pwd) !== false){
-        header("location: ../loginOwner.php?error=emptyinput");
+    require_once "dbh.inc.php";
+    require_once "functions.inc.php";
+
+    if (emptyInputLogIn($email, $pass)) {
+        header("Location: ../login.php?error=emptyinput");
+        if (emptyInputLogIn($email, $pwd) !== false) {
+            header("location: ../login.php?error=emptyinput");
+            exit();
+        }
+        if (invalidEmail($email)) {
+            header("Location: ../login.php?error=wronglogin");
+            exit();
+        }
+        loginUser($conn, $email, $pwd);
+    } else {
+        header("location: ../loginOwner.php");
         exit();
     }
-    if(invalidEmail($email)){
-        header("Location: ../loginOwner.php?error=wronglogin");
+
+    if (invalidEmail($email)) {
+        header("Location: ../login.php?error=wronglogin");
         exit();
     }
-    loginUser($conn, $email, $pwd);
-    header("Location: ../loginOwner.php?error=success");
-    exit();
-}
-else{
-    header("location: ../loginOwner.php");
-    exit();
-}
 
-if(invalidEmail($email)){
-    header("Location: ../login.php?error=wronglogin");
-    exit();
-}
+    if (!emailExistLogin($conn, $email)) {
+        header("Location: ../login.php?error=mailnotfound");
+        exit();
+    }
 
-if(!emailExistLogin($conn, $email)){
-    header("Location: ../login.php?error=mailnotfound");
-    exit();
+    header("Location: ../login.php");
 }
-
-header("Location: ../login.php");
