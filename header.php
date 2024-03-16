7b2d5afe119a5d2927ca0fe8f1290be8612b5c78
DESKTOP-060LROS\Inspira Grupa <dusanstanic2@gmail.com>
2024-03-16 11:38:33 +0100

    iconica dodata

--- header.php
+++ header.php
@@ -17,15 +17,16 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
 <body>
 <nav>
     <div class="navbar-wrapper fs-4 container-fluid">
-        <a href="index.php"><i class="bi bi-door-open"></i></a>
+        <a href="index.php"></i></a>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Cimeri</a>
+                <a class="navbar-brand" href="#"><i class="bi bi-house"></i></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
