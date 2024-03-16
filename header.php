a45674ce29d1229b477688e077957b179ef1ec46
Narmi <kujundzicmatej64@gmail.com>
2024-03-16 11:29:54 +0100

    Changed navbar color, added new layout.

--- header.php
+++ header.php
@@ -30,7 +30,8 @@ session_start();
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Update this ul to take the full width and justify the content to the center -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -40,12 +41,14 @@ session_start();
                         <li class="nav-item">
                             <a class="nav-link" href="#">Find Your Room</a>
                         </li>
+                        <li class="nav-item">
+                            <form class="d-flex py-2" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </li>
                     </ul>
-                    <form class="d-flex mx-auto" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-
+                    <li class="navbar-nav nav-item"><a class="nav-links" href="#">Login</a></li>
                 </div>
             </div>
         </nav>
