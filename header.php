fca2bb7f60173a1c1a899b99634d08cde9e4b2b6
Narmi <kujundzicmatej64@gmail.com>
2024-03-16 10:58:50 +0100

    Changed style, new navbar

--- header.php
+++ header.php
@@ -23,20 +23,38 @@ session_start();
 <nav>
     <div>
         <a href="index.php"><i class="bi bi-door-open"></i></a>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="discover.php">About Us</a></li>
-            <li><a href="blog.php">Find Blogs</a></li>
-            <?php
-                if (isset($_SESSION["useruid"])){
-                    echo "<li><a href=\"profile.php\">Profile page</a></li>";
-                    echo "<li><a href=\"includes/logout.inc.php\">Log out</a></li>";
-                }
-                else{
-                    echo "<li><a href=\"signup.php\">Sign Up</a></li>";
-                    echo "<li><a href=\"login.php\">Log In</a></li>";
-                }
-            ?>
-        </ul>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Cimeri</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
     </div>
 </nav>
