6397de1bf4d25dd1389ca2e01e9867833deaf461
IDK616\inspira <duledule17113@gmail.com>
2024-03-16 13:46:29 +0100

    Resolve conflict.

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,20 +1,14 @@
 <?php
-<<<<<<< Updated upstream
-=======
 if(isset($_POST["submit"])){
     $email = $_POST["uid"];
->>>>>>> Stashed changes
 
 $email = $_POST["email"];
 $pass = $_POST["pwd"];
-
-<<<<<<< Updated upstream
 require_once "dbh.inc.php";
 require_once "functions.inc.php";
 
 if(emptyInputLogIn($email,$pass)){
     header("Location: ../login.php?error=emptyinput");
-=======
     if(emptyInputLogIn($email, $pwd) !== false){
         header("location: ../loginOwner.php?error=emptyinput");
         exit();
@@ -29,7 +23,6 @@ if(emptyInputLogIn($email,$pass)){
 }
 else{
     header("location: ../loginOwner.php");
->>>>>>> Stashed changes
     exit();
 }
 
