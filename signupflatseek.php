eabc43bf30760a99f5dd4ba4c3ae4170b3b2d5f8
DESKTOP-060LROS\Inspira Grupa <dusanstanic2@gmail.com>
2024-03-17 06:10:35 +0100

    password restrict

--- signupflatseek.php
+++ signupflatseek.php
@@ -70,7 +70,8 @@ include_once "header.php"
                                         <div class="d-flex flex-row align-items-center mb-4">
                                             <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                             <div class="form-outline flex-fill mb-0">
-                                                <input type="password" name="password" id="formPassword" class="form-control" />
+                                                <input type="password" name="password" id="formPassword" class="form-control"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}"
+           title="Must contain at least one number and one uppercase and lowercase letter, one special character, and at least 8 or more characters" required> />
                                                 <label class="form-label" for="formPassword">Password</label>
                                             </div>
                                         </div>
