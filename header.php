ed2238713aa5fa80ca23d09c6c475a80fc3909c9
Narmi <kujundzicmatej64@gmail.com>
2024-03-17 08:30:23 +0100

    no message

--- header.php
+++ header.php
@@ -8,29 +8,11 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Find Your Perfect Roommate Today | RoomieMatch</title>
-    <meta name="title" content="Find Your Perfect Roommate Today | RoomieMatch">
-    <meta name="description" content="RoomieMatch helps you find the ideal roommate. Browse listings, connect with potential roommates, and find your perfect match. Start your roommate search today!">
 
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://cimi.com/">
-    <meta property="og:title" content="Find Your Perfect Roommate Today | RoomieMatch">
-    <meta property="og:description" content="RoomieMatch helps you find the ideal roommate. Browse listings, connect with potential roommates, and find your perfect match. Start your roommate search today!">
-    <meta property="og:image" content="https://cimi.com/images/share-image.jpg">
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://cimi.com/">
-    <meta property="twitter:title" content="Find Your Perfect Roommate Today | RoomieMatch">
-    <meta property="twitter:description" content="RoomieMatch helps you find the ideal roommate. Browse listings, connect with potential roommates, and find your perfect match. Start your roommate search today!">
-    <meta property="twitter:image" content="https://cimi.com/images/share-image.jpg">
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="https://cimi.com/path/to/favicon.png">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-
+    <meta name="title" content="Find Your Perfect Roommate Today | RoomieMatch">
+    <meta name="description" content="RoomieMatch helps you find the ideal roommate. Browse listings, connect with potential roommates, and find your perfect match. Start your roommate search today!">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
