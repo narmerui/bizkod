035a6a5bd4d296cdffe2f7b58c40f64d960f3bb9
Narmi <kujundzicmatej64@gmail.com>
2024-03-17 09:00:11 +0100

    no message

--- header.php
+++ header.php
@@ -11,8 +11,6 @@ session_start();
 
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
-    <meta name="title" content="Find Your Perfect Roommate Today | RoomieMatch">
-    <meta name="description" content="RoomieMatch helps you find the ideal roommate. Browse listings, connect with potential roommates, and find your perfect match. Start your roommate search today!">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
