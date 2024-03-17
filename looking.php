2dc698c8e8dca1c3cfa9b8e665863dfb53374b52
Narmi <kujundzicmatej64@gmail.com>
2024-03-17 04:23:32 +0100

    looking final design

--- looking.php
+++ looking.php
@@ -34,21 +34,30 @@ $result = $stmt->get_result();
     }
 
 </style>
-<main class="container mt-5 p-5" >
-    <div class="col-12">
-        <form action="looking.php" method="GET" class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search..." name="search" value="<?= htmlspecialchars($search); ?>">
-            <select class="form-select" name="column">
-                <option value="name" <?= $column === 'name' ? 'selected' : ''; ?>>Name</option>
-                <option value="city" <?= $column === 'city' ? 'selected' : ''; ?>>City</option>
-                <option value="description" <?= $column === 'description' ? 'selected' : ''; ?>>Description</option>
-            </select>
-            <input type="number" class="form-control" placeholder="Max price" name="maxPrice" value="<?= htmlspecialchars($maxPrice); ?>">
-            <button class="btn btn-primary" type="submit">Search</button>
+<main class="container-fluid mt-5 p-5 vh-100" >
+    <div class="card container col-12 p-3">
+        <form action="looking.php" method="GET" class="row g-3">
+            <div class="col-12">
+                <input type="text" class="form-control" placeholder="Search..." name="search" value="<?= htmlspecialchars($search); ?>">
+            </div>
+            <div class="col-12 col-sm-6 col-md">
+                <select class="form-select" name="column">
+                    <option value="name" <?= $column === 'name' ? 'selected' : ''; ?>>Name</option>
+                    <option value="city" <?= $column === 'city' ? 'selected' : ''; ?>>City</option>
+                    <option value="description" <?= $column === 'description' ? 'selected' : ''; ?>>Description</option>
+                </select>
+            </div>
+            <div class="col-12 col-sm-6 col-md">
+                <input type="number" class="form-control" placeholder="Max price" name="maxPrice" value="<?= htmlspecialchars($maxPrice); ?>">
+            </div>
+            <div class="col-12">
+                <button class="btn btn-primary w-100" type="submit">Search</button>
+            </div>
         </form>
     </div>
+
     </div>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4 py-2">
         <?php while ($row = $result->fetch_assoc()): ?>
             <div class="col">
                 <div class="card h-100"> <!-- Use h-100 to make cards of equal height -->
